ca875f01575826b05d8f31e789710a46ce66de15
silanoc <silanoc@gmail.com>
2022-08-11 15:56:38 +0200

    test de remplissage

--- test_modele_vie.py
+++ test_modele_vie.py
@@ -13,8 +13,16 @@ import pytest
 import os
 import modele_vie
 
+pytest.main(args=['-s', os.path.abspath('test_modele_vie.py')])
+
 def test_creation_automate():
     automate = modele_vie.Automate((3,3))
     assert automate.grille == [["", "", ""], ["", "", ""], ["", "", ""]]
 
-pytest.main(args=['-s', os.path.abspath('test_modele_vie.py')])
+def test_remplissage_aleatoire_a_la_creation():
+    automate = modele_vie.Automate((3,3))
+    remplissage = automate.remplissage_aleatoire_a_la_creation()
+    #remplissage = automate.grille
+    assert len(remplissage) == 3 and len(remplissage[0]) == 3
+
+
