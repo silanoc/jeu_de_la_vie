270d5be5edfbace2d7e8ffb011081b5ff4745acf
silanoc <silanoc@gmail.com>
2022-08-26 16:15:59 +0200

    meilleurs commentaires + pep8

--- jeu_de_la_vie/controleur_vie.py
+++ jeu_de_la_vie/controleur_vie.py
@@ -1,32 +1,60 @@
-#-------------------------------------------------------------------------------
-# Name:        controleur vie
-# Purpose:     dans une architecture MVC, contrôeleur du jeu de la vie.
-#
-# Author:      silanoc
-#
-# Created:     08/08/2022
-# Version :    1.1
-#-------------------------------------------------------------------------------
+#! /usr/bin/env python3
+# coding: utf-8
+
+"""dans une architecture MVC, contrôleur du jeu de la vie."""
 
 import jeu_de_la_vie.vue_vie as vue_vie
 import jeu_de_la_vie.modele_vie as modele_vie
 
+
 def creer_une_grille_initale(ligne, colonne):
-    grille_de_jeu = modele_vie.Automate((ligne,colonne))
+    """Création d'une grille rempli pour commencer.
+
+    A partir de la classe Automate du module modele_vie, crré une instance grille de jeu.
+    Chaque cellule à un état 0 ou 1.
+    Dit au module vue_vie d'exécuter la fonction nouvelle partie avec l'instance et ces valeurs
+
+    arg :
+    - ligne (int) : nombre de ligne que l'on souhaite pour la grille
+    - colonne (int) : nombre de colonne que l'on souhaite pour la grille
+    """
+    grille_de_jeu = modele_vie.Automate((ligne, colonne))
     grille_de_jeu.grille = grille_de_jeu.remplissage_aleatoire_a_la_creation()
     vue_vie.nouvelle_partie(grille_de_jeu.grille, grille_de_jeu)
 
+
 def gestion_choix_menu_principal(choix):
+    """Appliquer la decision demandé dans vue_vie.menu_principal()
+
+    Selon le choix, demande au module vue_vie d'exécuter une fonction différente.
+
+    arg:
+    - choix (string) : valeur détermnié dans vue_vie.menu_principal()
+    """
     if choix == "Nouvelle partie aléatoire":
         vue_vie.choix_taille_grille()
         #vue_vie.nouvelle_partie(affichage, grille_de_jeu)
     elif choix == "Quitter":
         vue_vie.quitter()
 
+
 def gestion_choix_taille(ligne, colonne):
     grille_de_jeu = creer_une_grille_initale(ligne, colonne)
 
+
 def gestion_demande_nouveau_tour(choix, grille, grille_de_jeu):
+    """Appliquer la decision demandé dans vue_vie.affiche_une_grille()
+
+    Selon la décision binaire
+    - recalcule un génération dans l'instance grille_de_jeu. 
+    Et demande à vue_vie de l'afficher.
+    - Demande à vue_vie de revenir au menu principal.
+
+    arg:
+    - choix (boolean) : déterminé dans vue_vie.affiche_une_grille()
+    - grille (liste de liste) : 0 ou 1 issus de l'instance grille_de_jeu
+    - grille_de_jeu (objet) : l'instance d'Automate
+    """
     if choix:
         grille_de_jeu.genere_nouvelle_generation()
         grille = grille_de_jeu.grille
--- jeu_de_la_vie/vue_vie.py
+++ jeu_de_la_vie/vue_vie.py
@@ -1,84 +1,122 @@
-#-------------------------------------------------------------------------------
-# Name:        vue vie
-# Purpose:     dans le modèle MVC le gestionnaire de vue.
-#
-# Author:      silanoc
-#
-# Created:     08/08/2022
-# Version :    1.1
-#-------------------------------------------------------------------------------
+#! /usr/bin/env python3
+# coding: utf-8
+
+"""dans le modèle MVC le gestionnaire de vue.
+
+Les choix de l'utilisateur sont fait par la bibliothèque questionary"""
 
 import time
 import sys
 import os
-#---
+
 import questionary
-#---
+
 import jeu_de_la_vie.controleur_vie as controleur_vie
+
 
 class Ecran():
     """Classe contenant toutes les zones de textes à afficher.
-    Permet d'alléger les fonctions de la vue."""
+    Permet d'alléger les fonctions de ce fichier vue_vie.
+    """
 
     def ecran_ouverture_jeu(self):
-        """Premier affichage à l'ouverture du programme"""
+        """Premier affichage à l'ouverture du programme
+
+        return:
+        - affiche (string)
+        """
         affiche = """Bienvenu sur mon programme
 
         Jeu de la vie
 
         by Silanoc - août 2022"""
-
         return affiche
 
     def ecran_menu(self):
+        """Message pour le menu principal.
+        return:
+        - affiche (string)
+        """
         affiche = "Vous avez plusieurs choix"
         return affiche
 
     def ecran_demande_taille_grille(self):
+        """Message pour demander la taille voulu pour la grille
+
+        return:
+        - affiche (string)
+        """
         affiche = """Quelle taille voulez-vous pour la grilel de jeu ?
         Merci de rentrer un nombre entier positif à chaque fois."""
         return affiche
 
     def ecran_debut_partie(self):
+        """Message pour le début de la partie, génération 0
+
+        return:
+        - affiche (string)
+        """
         affiche = "partie en cours"
         return affiche
 
     def ecran_quitter(self):
+        """Dernier affichage visible par l'utilisateur.
+
+        return:
+        - affiche (string)
+        """
         affiche = "Merci d'avoir utilisé ce programme. A une prochaine occasion."
         return affiche
 
-#-------------------------------------------------------------------------------
 
+# instantiation de l'objet écran.
 les_ecrans = Ecran()
 
+
 def efface_console():
-    """pour nettoyer la console entre chaque vue, et avoir quelque chose de propre"""
+    """Pour nettoyer la console entre chaque vue, et avoir quelque chose de propre"""
     if sys.platform.startswith("win"): #si windows
         os.system("cls")
-    else :
+    else:
         os.system("clear")
 
+
 def demarer_jeu():
-    """Première fonction appelée au démarage du jeu"""
+    """Première fonction appelée au démarage du jeu.
+
+    Affiche l'écran d'ouverture puis le menu.
+    """
     efface_console()
     affiche = les_ecrans.ecran_ouverture_jeu()
     print(affiche)
     time.sleep(1)
     menu_principal()
 
+
 def menu_principal():
+    """Menu principal.
+
+    Demande un choix à l'utilisateur (commencer une nouvelle partie ou quitter).
+    Transmet se choix (string) à la fonction gestion_choix_menu_principal du module controleur_vie.
+    """
     efface_console()
     affiche = les_ecrans.ecran_menu()
     print(affiche)
     choix = questionary.select("Que voulez-vous faire ?", choices = ['Nouvelle partie aléatoire', 'Quitter']).ask()
     controleur_vie.gestion_choix_menu_principal(choix)
 
+
 def choix_taille_grille():
+    """L'utilisateur choisi la taille de la grille de jeu
+
+    Demande à l'utilisateur deux nombres entiers positifs ligne et colonne.
+    Les transmet à la fonction gestion_choix_taille du module controleur_vie.
+    """
     efface_console()
     affiche = les_ecrans.ecran_demande_taille_grille()
     print(affiche)
     ok = False
-    while  ok == False or ligne < 0 :
+    while  ok is False or ligne < 0 :
         ligne = questionary.text("nombre de ligne").ask()
         try:
             ligne = int(ligne)
@@ -86,7 +124,7 @@ def choix_taille_grille():
         except:
             ok = False
     ok = False
-    while ok == False or colonne < 0:
+    while ok is False or colonne < 0:
         colonne = questionary.text("nombre de colonne").ask()
         try:
             colonne = int(colonne)
@@ -95,15 +133,27 @@ def choix_taille_grille():
             ok = False
     controleur_vie.gestion_choix_taille(ligne, colonne)
 
+
 def nouvelle_partie(grille, grille_de_jeu):
+    """Affiche la génération génération 0
+
+    arg :
+    - grille (liste de liste) : 0 ou 1 issus de l'instance grille_de_jeu
+    - grille_de_jeu (objet) : l'instance d'Automate
+    """
     efface_console()
     affiche = les_ecrans.ecran_debut_partie()
     print(affiche)
     affiche_une_grille(grille, grille_de_jeu)
 
+
 def affiche_une_grille(grille, grille_de_jeu):
-    """affiche dans la consolle la grille
-        pour plus de lisibilité, espace vide si 0 et X si vivant"""
+    """Affiche dans la consolle l'état des cellules.
+    Pour plus de lisibilité, " " si 0/mort et "X" si 1/vivant
+
+    - grille (liste de liste) : 0 ou 1 issus de l'instance grille_de_jeu
+    - grille_de_jeu (objet) : l'instance d'Automate
+    """
     efface_console()
     for ligne in range(grille_de_jeu.nb_ligne):
         affiche = ""
@@ -117,7 +167,9 @@ def affiche_une_grille(grille, grille_de_jeu):
     choix = questionary.confirm("Encore un tour ?", default = True).ask()
     controleur_vie.gestion_demande_nouveau_tour(choix, grille, grille_de_jeu)
 
+
 def quitter():
+    """Affiche le message final et quitte le programme."""
     efface_console()
     affiche = les_ecrans.ecran_quitter()
     print(affiche)
