0d433b3a2c4a655a73b729178689d03ec2fc951a
silanoc <silanoc@gmail.com>
2022-09-02 10:20:15 +0200

    ajout d'un test qui loupe (TDD red) et correction des parametre du fichier

--- tests/test_vue_vie.py
+++ tests/test_vue_vie.py
@@ -5,10 +5,10 @@
 
 import pytest
 import os
-import jeu_de_la_vie.vue_vie as modele_vie
+import jeu_de_la_vie.vue_vie as vue_vie
 
 # Ligne nécessaire pour certaines configurations
-pytest.main(args=['-s', os.path.abspath('test_modele_vie.py')])
+pytest.main(args=['-s', os.path.abspath('test_vue_vie.py')])
 
 class Testvue_vie():
     """Objet contenant tous les tests."""
@@ -16,9 +16,13 @@ class Testvue_vie():
     def test_existance_Ecran(self) -> None:
         """Vérifie l'existance de la classe Ecran, et son __init__. 
         Vérifie que chacune des méthode génére un str autre qu'une chaine vide."""
-        ecran = modele_vie.Ecran()
+        ecran = vue_vie.Ecran()
         assert type(ecran.ecran_ouverture_jeu()) == str and ecran.ecran_ouverture_jeu() != ""
         assert type(ecran.ecran_menu()) == str and ecran.ecran_menu() != ""
         assert type(ecran.ecran_demande_taille_grille()) == str and ecran.ecran_demande_taille_grille() != ""
         assert type(ecran.ecran_debut_partie()) == str and ecran.ecran_debut_partie() != ""
         assert type(ecran.ecran_quitter()) == str and ecran.ecran_quitter() != ""
+        
+    def test_demande_int_superieur_zero(self) -> None:
+        nb = vue_vie.demande_int_superieur_zero()
+        assert type(nb) == int and nb > 0
